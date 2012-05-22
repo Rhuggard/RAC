bbbdd82f2b2a27e0af521e2152d118dc5f21c15c
Ronan Huggard <ronanHuggard@gmail.com>
2012-05-22 20:19:22 +0200

    initial commit: resultof: rails new thesisPOC

---
+++ thesisPOC/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThesisPOC::Application.config.secret_token = '86f854e8e81a34b1a4bf902e9802edcea530b6aa390ed4ed15cc223e8d426ec8f584774245ecb50d6d058e638f9d81d66a69236c1a20d8471c19671880445975'
