b1de9eab616a33988ebe82f9b36cd1f34fa4b166
Ronan Huggard <ronanHuggard@gmail.com>
2012-06-08 12:53:57 +0200

    Create a policy object for the owner of new files

--- thesisPOC/app/models/pobject.rb
+++ thesisPOC/app/models/pobject.rb
@@ -1,3 +1,9 @@
 class Pobject < ActiveRecord::Base
+  after_create :create_owner_permission
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  
+  def create_owner_permission
+    current_id = User.where(:name => 'user2').first
+    Policy.create!(:user_id => current_id.id, :action => 'view', :object_id => id)
+  end
 end
