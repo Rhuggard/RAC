12bc614eea9620c1aaca8245ed72b8ff0d631fe9
Ronan Huggard <ronanHuggard@gmail.com>
2012-07-01 16:20:20 +0200

    Added request and reply stubs to server
    Currently only can send request message to self.

--- nodeStack/nowServer.js
+++ nodeStack/nowServer.js
@@ -6,9 +6,33 @@ var nowjs = require("now");
 var everyone = nowjs.initialize(server);
 
 everyone.now.distributeMessage = function(message){
+  console.log("");
+  console.log("Sending-Message: " + this.now.name + " : " +  message);
   everyone.now.receiveMessage(this.now.name, message);
 };
 
+everyone.now.sendRequest = function( target, pobject, message ){
+  console.log("");
+  console.log("Sending request");
+  console.log("From : " + this.now.name);
+  console.log("To : " + target);
+  console.log("For object : " + pobject);
+  console.log("Additional Info :" + message);
 
+  if(this.now.name == target){
+    console.log("target aquired");
+    this.now.requestStack(message);
+  }
+  //Add group name
+}
+
+everyone.now.replyRequest = function(name, target, pobject, response, message){
+  console.log("Sending request");
+  console.log("From : " + this.now.name);
+  console.log("To : " + target);
+  console.log("For object : " + pobject);
+  console.log("Additional Info :" + message);
+  //Reply
+}
 
 
