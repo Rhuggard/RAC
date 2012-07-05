ef61f5f3835b86f75cd810ead0e414ab3aacbd21
Ronan Huggard <ronanHuggard@gmail.com>
2012-07-05 16:40:47 +0200

    Add owner permission upon creation of new object

--- thesisPOC/app/models/pobject.rb
+++ thesisPOC/app/models/pobject.rb
@@ -1,9 +1,13 @@
 class Pobject < ActiveRecord::Base
-  after_create :create_owner_permission
+  after_commit :create_owner_permission
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   attr_accessor :access 
   def create_owner_permission
-    current_id = User.where(:name => 'user2').first
+    ActiveRecord::Base.logger.debug "*************************************"
+    ActiveRecord::Base.logger.debug "self: #{self}"
+    ActiveRecord::Base.logger.debug "self.owner: #{self.owner}"
+    ActiveRecord::Base.logger.debug "*************************************"
+    current_id = User.where(:name => self.owner).first
     Policy.create!(:user_id => current_id.id, :action => 'view', :object_id => id)
   end
 
